32a6954a055cf2adab921b3fb1c27e9cd9ac19e1
Lan Wy Bui <lan.wy.bui2000@gmail.com>
2025-04-28 14:59:43 +0200

    push everything

--- olist/data.py
+++ olist/data.py
@@ -14,10 +14,31 @@ class Olist:
             # Use __file__ instead as an absolute path anchor independant of your usename
             # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
         # Hint 2: Use os.path library to construct path independent of Mac vs. Unix vs. Windows specificities
-        pass  # YOUR CODE HERE
+        project_path = os.path.dirname(__file__)
+        csv_path = os.path.join(project_path, '..', 'data','csv')
+        file_names = os.listdir(csv_path)
+        file_names = [file for file in file_names if file.endswith('.csv')]
+        key_names = []
+        for file in file_names:
+            if file[:5] == 'olist':
+                file = file[6:]
+            if "_dataset.csv" in file:
+                file=file.replace("_dataset.csv", "")
+            if ".csv" in file:
+                file=file.replace(".csv", "")
+            key_names.append(file)
+        data = {}
+        for (key, file_name) in zip(key_names, file_names):
+            data[key] = pd.read_csv(os.path.join(csv_path, file_name))
+        return data
+
+
 
     def ping(self):
         """
         You call ping I print pong.
         """
         print("pong")
+
+ol = Olist()
+print(ol.get_data())
